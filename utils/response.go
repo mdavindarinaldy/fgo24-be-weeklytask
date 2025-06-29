5fe9ad2e1cb338a14c855aec286623502b127733
Naldy <mdavindarinaldy@gmail.com>
2025-06-29 17:40:34 +0700

    feat: add response users struct to not include user's password and pin

--- utils/response.go
+++ utils/response.go
@@ -7,3 +7,10 @@ type Response struct {
 	PageInfo any    `json:"pageInfo,omitempty"`
 	Result   any    `json:"results,omitempty"`
 }
+
+type ResponseUsers struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
+}
